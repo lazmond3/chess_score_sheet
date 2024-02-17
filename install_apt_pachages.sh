d99375cf0be400571b048d0d0818bcd22e70aa93
Naoki.Yasuno <Naoki.Yasuno@sony.com>
2024-02-17 14:20:06 +0900

    Add/Remove/Ignore some util files

---
+++ install_apt_pachages.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -xue
+
+sudo apt -y install libopencv-dev \
+                    libopencv-core-dev \
+                    python3-opencv \
+                    libopencv-contrib-dev \
+                    opencv-data
